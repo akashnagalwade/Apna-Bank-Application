a5e671120e0831f16c06eb2ad33b8ec2016b11c1
Akash Nagalwade <akashnagalwade444@gmail.com>
2024-09-06 22:17:50 +0530

    initial change

---
+++ src/main/java/com/mindspark/service/BankStatement.java
@@ -0,0 +1,18 @@
+package com.mindspark.service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BankStatement {
+
+//    retrieve the list of transaction within a date range given an account number
+//    generate a pdf file of transactions
+//    save the file via email
+
+
+}
